ba5450bb6b075bd4343471e43e5394f30b583716
Aarav Leekha <105072452+aaravleekha@users.noreply.github.com>
2022-05-26 23:18:42 +0000

    Completed and tested parts 1-5.

--- part5.py
+++ part5.py
@@ -23,3 +23,14 @@ not a month
 '''
 
 #start writing your code below
+
+month = input("Enter a month in lowercase:")
+
+if month == "january" or "march" or "may" or "july" or "august" or "october" or "december":
+  print ("31")
+elif month == "april" or "june" or "september" or "november":
+  print ("30")
+elif month == "february":
+  print ("28 or 29")
+else:
+  print ("That's not a month")
